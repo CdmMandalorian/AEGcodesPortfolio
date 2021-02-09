6dc19ff57e78e9873e3212aec1692b9d27bc2701
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-02-09 15:21:35 -0800

    Finale Spacing for LA

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,8 +9,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Welcome Everyone, I am <span className="green">Armando  E  Galindo </span>
-            from <span className="green"> ✨ Los Angeles, California 🌙</span>
+            Welcome Everyone, I am <span className="green">  Armando  E  Galindo  </span>
+                  from 
+            <br></br>
+            <span className="green"> ✨ Los Angeles, California 🌙</span>
             <br />A Full Stack Web Developer, currently enrolled within UCR.  Persuing my passion, of learning, forging, and innovating with my creativity, dynamic based: Full Web Applications.
             <br />
             <br />
