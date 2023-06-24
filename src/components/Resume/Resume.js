a25b70b677fc13dc8a2cff1a1ba1b55caa118e2c
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2023-06-24 09:15:35 -0700

    latest updated resume added

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import Resumecontent from "./ResumeContent";
-import "../../style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Particle from '../Particle';
+import Resumecontent from './ResumeContent';
+import '../../style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import pdf from "../../Assets/AEGTechCodesResume23.6.pdf";
+import pdf from '../../Assets/AEGTechCodesResume23.7.3.pdf';
 
 function Resume() {
-  
-
   return (
     <Container fluid className="resume-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button variant="primary" href={pdf} target="_blank">
             <i className="fas fa-download">&nbsp;</i>Download CV
           </Button>
@@ -27,29 +25,29 @@ function Resume() {
               title="Manager | Price Coordinator [ Staples ]"
               date="February 2022 - "
               content={[
-                "Monitor, track, appropriate prices in the store according to sales and corporate. Secure accuracy of prices. ",
-                "Oversee operations in store, maintaining daily responsibilities, ensuring efficient workflow. ",
-                "Nurtured employee confidence, and strong credibility",
-                "Recognized for achieving optimal results in Sales and Customer Service ",
+                'Monitor, track, appropriate prices in the store according to sales and corporate. Secure accuracy of prices. ',
+                'Oversee operations in store, maintaining daily responsibilities, ensuring efficient workflow. ',
+                'Nurtured employee confidence, and strong credibility',
+                'Recognized for achieving optimal results in Sales and Customer Service ',
               ]}
             />
             <Resumecontent
               title="Project Manager | Continuous Improvement [ Ruskin ]"
               date="February 2019 - March 2020"
               content={[
-                "Analyzation of work procedures, to a determination, for the best safety protocols. ",
-                "Adapted an elementary understanding of ISO 9001 Quality Management systems, to serve the customer with best service while understanding and overcoming obstacles that might hinge on customer satisfaction. ",
-                "Effectively formulate strategies, tactics and action plans to achieve results. ",
-                "Provide high productive output while maintaining high morale . ",
+                'Analyzation of work procedures, to a determination, for the best safety protocols. ',
+                'Adapted an elementary understanding of ISO 9001 Quality Management systems, to serve the customer with best service while understanding and overcoming obstacles that might hinge on customer satisfaction. ',
+                'Effectively formulate strategies, tactics and action plans to achieve results. ',
+                'Provide high productive output while maintaining high morale . ',
               ]}
             />
             <Resumecontent
               title="Manager | Front End [ Kmart ]"
               date="September 2012 - December 2018"
               content={[
-                "Lead, organized, and trained cashiers to the highest of customer ratings. ",
-                "Manage employees to maintain a clean and friendly work environment. ",
-                "Resolve all customer complaints " ,
+                'Lead, organized, and trained cashiers to the highest of customer ratings. ',
+                'Manage employees to maintain a clean and friendly work environment. ',
+                'Resolve all customer complaints ',
               ]}
             />
           </Col>
@@ -58,24 +56,26 @@ function Resume() {
             <Resumecontent
               title=" [ University of California, Riverside ]"
               date="2020 - 2021"
-              content={["Full Stack Web Development"]}
+              content={['Full Stack Web Development']}
             />
             <Resumecontent
               title=" [ Chaffey College, Rancho Cucamonga ]"
               date="2010 - 2018"
-              content={["Associate of Science in Mathematics, University Studies"]}
+              content={[
+                'Associate of Science in Mathematics, University Studies',
+              ]}
             />
             <Resumecontent
               title=" [ Ontario High School ] "
               date="2004 - 2008"
-              content={["Diploma"]}
+              content={['Diploma']}
             />
             <h3 className="resume-title">Extracurricular Activities</h3>
             <Resumecontent
               title="Soccer Coach | Manager [ Ontario United ]"
               date="2011 - 2016"
               content={[
-                "Managed operations, and victoriously coached a succesful youth club soccer team. Traveling abroad participating in tournaments while triumphantly representing the city of Ontario. Achieving proper resources needed to attain maximum team results. Hosting the enviroment to develop and obtain the ultimate impact for each individual. Excel in obtaining full parental support. Adhere and maintain high ethical standards and policies for all involved.",
+                'Managed operations, and victoriously coached a succesful youth club soccer team. Traveling abroad participating in tournaments while triumphantly representing the city of Ontario. Achieving proper resources needed to attain maximum team results. Hosting the enviroment to develop and obtain the ultimate impact for each individual. Excel in obtaining full parental support. Adhere and maintain high ethical standards and policies for all involved.',
               ]}
             />
             <Resumecontent
@@ -90,22 +90,22 @@ function Resume() {
               title="AYSO Region 66 [ Ontario / Montclair ] "
               date="2011 - 2016 | 2021 "
               content={[
-                "Coach of the Year: 2012, 2013, 2014",
-                "Select Coach: U12 Boys 2012",
-                "Select Coach: U14 Boys 2014",
-                "Select Assistant Coach: U16 Boys 2016"
+                'Coach of the Year: 2012, 2013, 2014',
+                'Select Coach: U12 Boys 2012',
+                'Select Coach: U14 Boys 2014',
+                'Select Assistant Coach: U16 Boys 2016',
               ]}
             />
             <h3 className="resume-title">Achivements</h3>
             <Resumecontent
               title=""
               content={[
-                "Treasury - Foreign Language Club - Ontario High School"
+                'Treasury - Foreign Language Club - Ontario High School',
               ]}
             />
           </Col>
         </Row>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button variant="primary" href={pdf} target="_blank">
             <i className="fas fa-download">&nbsp;</i>Download CV
           </Button>
